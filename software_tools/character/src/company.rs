fec341703f6f7868a949839378e812ae56fa4fac
Galileo Alighieri <galileo.alighieri.dev@gmail.com>
2024-09-27 21:08:26 +0200

    Content removal

--- software_tools/character/src/company.rs
+++
@@ -1,8 +0,0 @@
-use super::*;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Company {
-    pub characters: Vec<CharacterProfile>,
-    pub relationships: Vec<String>,
-    pub additional_assets: Vec<Asset>,
-}
